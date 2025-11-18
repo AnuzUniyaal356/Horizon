a3f1368f4c062dc13d8ed0b0d1661486f87cd694
AnuzUniyaal <anuzuniyaal5313@gmail.com>
2025-11-18 23:32:32 +0530

    Implement production environment setup in index.js
    Added production environment handling and static file serving.

--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,20 @@ import cors from 'cors'
 import authRouter from './route/authRoute.js'
 import userRouter from './route/userRoute.js'
 import contentRouter from './route/contentRoute.js'
+import path from "path";
 
 dotenv.config()
 const port = process.env.PORT
+const __dirname = path.resolve();
+
+const NODE_ENV = "production";
 
 
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-   origin:"*",
+   origin:process.env.CLIENT_URL,
    credentials:true
 }))
 
@@ -28,6 +32,16 @@ app.use("/api/content",contentRouter)
 app.get("/" , (req,res)=>{
     res.send("Hello from Server")
 })
+
+
+// make our app ready for deployment
+if (NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port , ()=>{
     console.log("Server Started")
